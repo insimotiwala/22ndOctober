0323fefae6140e44da3997b5347529f58a6b2332
insimotiwala <86950756+insimotiwala@users.noreply.github.com>
2021-10-21 19:44:50 -0700

    initial commit

---
+++ Assets/MyScripts/Factorycolor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factorycolor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("South"))
+        {
+            this.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+}
